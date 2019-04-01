3f48781402570c3ff6c187ecfc94bdd81178792f
Henrik <skinnybeans@gmail.com>
2019-04-02 08:00:25 +1100

    restyled investigator list so only 1 is visible at once

--- src/app/investigator-list/investigator-list.component.ts
+++ src/app/investigator-list/investigator-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Investigator } from '../investigator/investigator.model';
 
+
 @Component({
   selector: 'app-investigator-list',
   templateUrl: './investigator-list.component.html',
@@ -9,14 +10,42 @@ import { Investigator } from '../investigator/investigator.model';
 })
 export class InvestigatorListComponent implements OnInit {
 
-  // TODO: pass this data to the investigator component
   investigators: Investigator[] = [
     new Investigator('Wendy Adams', 0 , 1, ['nothing of note']),
     new Investigator('Roland Banks', 1 , 1, ['relic of ages'])
   ];
+
+  readonly maxInvestigators = 4;
+
+  selectedInvestigator: Investigator;
+  selectedIndex: number = undefined;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggleInvestigator(id: number, event: any) {
+    if (this.selectedInvestigator === this.investigators[id]) {
+      this.selectedInvestigator = undefined;
+      this.selectedIndex = undefined;
+    } else {
+      this.selectedInvestigator = this.investigators[id];
+      this.selectedIndex = id;
+    }
+  }
+
+  onDeleteInvestigator() {
+    if (this.selectedIndex !== undefined) {
+      this.investigators.splice(this.selectedIndex, 1);
+      this.selectedInvestigator = undefined;
+      this.selectedIndex = undefined;
+    }
+  }
+
+  onAddInvestigator() {
+    this.investigators.push(new Investigator('New Investigator'));
+    this.selectedIndex = this.investigators.length - 1;
+    this.selectedInvestigator = this.investigators[this.selectedIndex];
+  }
 }
