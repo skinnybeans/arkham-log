ff2b95814deee2cea1e757c25207462711fb5292
Henrik <skinnybeans@gmail.com>
2019-04-08 18:22:41 +1000

    adding model to hold mission stats and sequence them

--- src/app/campaign-progress/campaign-progress.component.ts
+++ src/app/campaign-progress/campaign-progress.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Mission } from './mission.model';
+
 @Component({
   selector: 'app-campaign-progress',
   templateUrl: './campaign-progress.component.html',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampaignProgressComponent implements OnInit {
 
+  // Going to need something for campaign specific notes
   notes: string[] = ['some note about the campaign so far...'];
-  constructor() { }
+  missions: Mission[] = [
+    {name: 'mission 2', completed: false, experience: 0, sequence: 2},
+    {name: 'mission 1', completed: false, experience: 0, sequence: 1},
+    {name: 'mission 3', completed: false, experience: 0, sequence: 3},
+  ];
+
+
+  constructor() {
+    // Sort the mission array here for now, will need to move later
+    this.missions.sort((a: Mission, b: Mission) => {
+      return a.sequence - b.sequence;
+    });
+   }
 
   ngOnInit() {
   }
---
+++ src/app/campaign-progress/mission.model.ts
@@ -0,0 +1,7 @@
+export class Mission {
+    constructor(
+        public name: string,
+        public completed: boolean,
+        public experience: number,
+        public sequence: number) {}
+}
