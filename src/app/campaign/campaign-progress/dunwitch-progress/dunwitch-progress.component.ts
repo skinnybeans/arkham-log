75dec84e9749edde5bc3f305f0ab5ed6e801cc56
Henrik <skinnybeans@gmail.com>
2019-06-24 08:27:10 +1000

    added dunwitch sacrificed list

--- src/app/campaign/campaign-progress/dunwitch-progress/dunwitch-progress.component.ts
+++ src/app/campaign/campaign-progress/dunwitch-progress/dunwitch-progress.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DunwitchProgressComponent implements OnInit {
 
+  sacrificed: string = '';
+  sacrificedList: string[] = ['Captain Jack', 'Giant Squid'];
   constructor() { }
 
   ngOnInit() {
