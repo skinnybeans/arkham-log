cff04646ea05186de6b7bcd51ac15572b20715b6
Henrik <skinnybeans@gmail.com>
2019-06-24 12:21:12 +1000

    update investigator list page styling
    - added icons
    - changed the elements from links to buttons

--- src/app/campaign/campaign-progress/dunwitch-progress/dunwitch-progress.component.ts
+++ src/app/campaign/campaign-progress/dunwitch-progress/dunwitch-progress.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DunwitchProgressComponent implements OnInit {
 
-  sacrificed: string = '';
+  sacrificed = '';
   sacrificedList: string[] = ['Captain Jack', 'Giant Squid'];
   constructor() { }
 
