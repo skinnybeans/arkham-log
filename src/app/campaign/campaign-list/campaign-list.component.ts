575b97cd321aae101ad25a0cc21ac46f80f14091
Henrik <skinnybeans@gmail.com>
2019-07-08 11:46:45 +1000

    added some hardcoded reading writing to firebase

--- src/app/campaign/campaign-list/campaign-list.component.ts
+++ src/app/campaign/campaign-list/campaign-list.component.ts
@@ -5,7 +5,9 @@ import {
 } from '@angular/core';
 
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import {
+  Subscription,
+  from} from 'rxjs';
 
 import {
   FormGroup,
@@ -15,7 +17,9 @@ import {
 
 import {
   AngularFirestore,
-  AngularFirestoreDocument
+  AngularFirestoreDocument,
+  AngularFirestoreCollection,
+  AngularFirestoreModule
 } from '@angular/fire/firestore';
 
 import { Campaign, CampaignType } from '../campaign.model';
@@ -37,7 +41,9 @@ export class CampaignListComponent implements OnInit, OnDestroy {
     type: new FormControl(null, [Validators.required])
   });
 
+  // Testing the firestore
   campaignDoc: AngularFirestoreDocument<Campaign>;
+  campaignCollection: AngularFirestoreCollection<Campaign>;
 
   constructor(
     private router: Router,
@@ -50,6 +56,27 @@ export class CampaignListComponent implements OnInit, OnDestroy {
     this.campaignSub = this.campaignService.campaignsChanged.subscribe(
       (campaigns: Campaign[]) => {
         this.campaigns = campaigns;
+      }
+    );
+
+    this.angularFirestore.collection<Campaign>('campaigns').valueChanges({idField: '_id'}).subscribe(
+      data => {
+        console.log('Reading data');
+        console.log(data);
+      }
+    );
+
+    // writing data
+    from(this.angularFirestore.collection<Campaign>('campaigns').doc('XMRoNhBQwIOHKTChTSeA').set({name: 'setting through app'}, {merge: true})).subscribe(
+      res => {
+        console.log('update success');
+        console.log(res);
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        console.log('update finished');
       }
     );
   }
@@ -65,13 +92,31 @@ export class CampaignListComponent implements OnInit, OnDestroy {
     this.campaignService.addCampaign(new Campaign(CampaignType[campaignType], campaignName));
     this.campaignForm.reset();
 
-    this.angularFirestore.collection('campaigns').add(this.campaigns[0]).then(
-      res => {
-        console.log('Saved');
+    // Observable way
+    from(this.angularFirestore.collection('campaigns').add(this.campaigns[0])).subscribe(
+      result => {
+        console.log('result');
+        console.log(result);
       },
-      reject => {
-        console.log(`Didn't save`);
+      error => {
+        console.log('error');
+        console.log(error);
+      },
+      () => {
+        console.log('complete');
       }
     );
+
+    // Promise way
+    // this.angularFirestore.collection('campaigns').add(this.campaigns[0]).then(
+    //   res => {
+    //     console.log('Observe Saved');
+    //     console.log(res);
+    //   },
+    //   reject => {
+    //     console.log(`Observe Error`);
+    //     console.log(reject);
+    //   }
+    // );
   }
 }
