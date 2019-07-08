ce539c5bb0166022f06a17a4879ea89765d66aa0
Henrik <skinnybeans@gmail.com>
2019-07-08 16:44:03 +1000

    persisting campaigns to angular firestore
    campaign adding and deleting will now persist to firestore

--- src/app/campaign/campaign-list/campaign-list.component.ts
+++ src/app/campaign/campaign-list/campaign-list.component.ts
@@ -4,23 +4,15 @@ import {
   OnDestroy
 } from '@angular/core';
 
-import { Router } from '@angular/router';
 import {
-  Subscription,
-  from} from 'rxjs';
+  Subscription
+} from 'rxjs';
 
 import {
   FormGroup,
   FormControl,
   Validators
 } from '@angular/forms';
-
-import {
-  AngularFirestore,
-  AngularFirestoreDocument,
-  AngularFirestoreCollection,
-  AngularFirestoreModule
-} from '@angular/fire/firestore';
 
 import { Campaign, CampaignType } from '../campaign.model';
 import { CampaignService } from '../campaign.service';
@@ -35,94 +27,46 @@ import { CampaignService } from '../campaign.service';
 export class CampaignListComponent implements OnInit, OnDestroy {
 
   campaigns: Campaign [];
-  campaignSub: Subscription;
+  campaignSubs: Subscription = new Subscription();
   campaignForm: FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required]),
     type: new FormControl(null, [Validators.required])
   });
 
-  // Testing the firestore
-  campaignDoc: AngularFirestoreDocument<Campaign>;
-  campaignCollection: AngularFirestoreCollection<Campaign>;
-
   constructor(
-    private router: Router,
-    private campaignService: CampaignService,
-    private angularFirestore: AngularFirestore) { }
+    private campaignService: CampaignService
+  ) {}
 
   ngOnInit() {
     this.campaigns = this.campaignService.getCampaigns();
 
-    this.campaignSub = this.campaignService.campaignsChanged.subscribe(
+    this.campaignSubs.add(this.campaignService.campaignsChanged.subscribe(
       (campaigns: Campaign[]) => {
         this.campaigns = campaigns;
       }
-    );
-
-    this.angularFirestore.collection<Campaign>('campaigns').valueChanges({idField: '_id'}).subscribe(
-      data => {
-        console.log('Reading data');
-        console.log(data);
-      }
-    );
-
-    // writing data
-    from(
-      this.angularFirestore
-        .collection<Campaign>('campaigns')
-        .doc('XMRoNhBQwIOHKTChTSeA')
-        .set({name: 'setting through app'}, {merge: true})
-    )
-      .subscribe(
-        res => {
-          console.log('update success');
-          console.log(res);
-        },
-        error => {
-          console.log(error);
-        },
-        () => {
-          console.log('update finished');
-        }
-      );
+    ));
   }
 
   ngOnDestroy() {
-    this.campaignSub.unsubscribe();
+    this.campaignSubs.unsubscribe();
+  }
+
+  onDelete(id: string) {
+    this.campaignService.deleteCampaign(id);
   }
 
   onSubmit() {
     const campaignName: string = this.campaignForm.get('name').value;
     const campaignType: keyof typeof CampaignType = this.campaignForm.get('type').value;
 
-    this.campaignService.addCampaign(new Campaign(CampaignType[campaignType], campaignName));
-    this.campaignForm.reset();
-
-    // Observable way
-    from(this.angularFirestore.collection('campaigns').add(this.campaigns[0])).subscribe(
-      result => {
-        console.log('result');
-        console.log(result);
-      },
-      error => {
-        console.log('error');
-        console.log(error);
-      },
-      () => {
-        console.log('complete');
-      }
+    this.campaignSubs.add(
+      this.campaignService.addCampaign(new Campaign(CampaignType[campaignType], campaignName)).subscribe(
+        result => {
+          console.log('finished adding campaign');
+          console.log(result);
+          this.campaignForm.reset();
+        }
+      )
     );
-
-    // Promise way
-    // this.angularFirestore.collection('campaigns').add(this.campaigns[0]).then(
-    //   res => {
-    //     console.log('Observe Saved');
-    //     console.log(res);
-    //   },
-    //   reject => {
-    //     console.log(`Observe Error`);
-    //     console.log(reject);
-    //   }
-    // );
   }
 }
--- src/app/campaign/campaign.model.ts
+++ src/app/campaign/campaign.model.ts
@@ -4,11 +4,10 @@ export enum CampaignType {
 }
 
 export class Campaign {
-    public name: string;
-    public campaignType: CampaignType;
 
-    constructor(campaignType: CampaignType, name: string) {
-        this.campaignType = campaignType;
-        this.name = name;
+    constructor(
+        public campaignType: CampaignType,
+        public name: string,
+        public id?: string) {
     }
 }
